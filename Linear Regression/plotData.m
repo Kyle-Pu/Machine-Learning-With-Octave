bfb8900772fddec72950276e02a53224968e8f03
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-07-23 11:24:00 -0700

    Create plotData.m

---
+++ Linear Regression/plotData.m
@@ -0,0 +1,7 @@
+function plotData(x, y)
+
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data, "rx" = red 'x' as data points, markersize is set to 10
+ylabel('Profit in $10,000s'); % Set the y-axis label
+xlabel('Population of City in 10,000s'); % Set the x-axis label
+
+endfunction
