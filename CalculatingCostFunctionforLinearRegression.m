c5e36dc97e70394eee51e8d5eb4bce4e77df1004
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-07-22 22:35:09 -0700

    Add files via upload

---
+++ CalculatingCostFunctionforLinearRegression.m
@@ -0,0 +1,17 @@
+function J = CalculatingCostFunctionsforLinearRegression(X, y, theta)
+
+%J will be returned
+
+%X = matrix of x-values
+%y = vector y-values
+%theta = matrix of parameters for linear regression
+
+m = length(y); %number of training examples
+
+hypotheses = X * theta;     %Calculates vector of hypotheses
+errors = hypotheses - y;    %Calculates vector of errors
+squaredErrors = errors.^2;  %Calculates element-wise squared errors
+
+J = (1/(2*m))*(sum(squaredErrors));  %Calculates and returns the cost function
+
+endfunction
