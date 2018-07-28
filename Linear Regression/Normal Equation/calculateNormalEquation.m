728fc232553df4f9af2a0d4aed318b99899ccc1d
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-07-27 22:37:21 -0700

    Update calculateNormalEquation.m

--- Linear Regression/Normal Equation/calculateNormalEquation.m
+++ Linear Regression/Normal Equation/calculateNormalEquation.m
@@ -1,3 +1,12 @@
+%% Load Data
+data = csvread('ex1data2.txt');
+X = data(:, 1:2);
+y = data(:, 3);
+m = length(y);
+
+% Add intercept term to X
+X = [ones(m, 1) X];
+
 % Calculate the parameters from the normal equation
 theta = normalEquation(X, y);
 
