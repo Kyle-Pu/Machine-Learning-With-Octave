6b912cd888b8d0f2a280b7bf72c578d825d7ea10
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-07-27 22:36:00 -0700

    Create calculateNormalEquation.m

---
+++ Linear Regression/Normal Equation/calculateNormalEquation.m
@@ -0,0 +1,7 @@
+% Calculate the parameters from the normal equation
+theta = normalEquation(X, y);
+
+% Display normal equation's result
+fprintf('Theta computed from the normal equations: \n');
+fprintf(' %f \n', theta);
+fprintf('\n');
