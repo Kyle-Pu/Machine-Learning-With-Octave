bdd9993947d11ce05fb5150ffe246269034a2ff9
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-07-27 22:35:02 -0700

    Create linearRegressMultivariable.m

---
+++ Linear Regression/Multivariate/linearRegressMultivariable.m
@@ -0,0 +1,37 @@
+%% Load Data
+data = load('ex1data2.txt');
+X = data(:, 1:2);
+y = data(:, 3);
+m = length(y);
+
+[X mu sigma] = featureNormalization(X);
+
+% Add intercept term to X
+X = [ones(m, 1) X];
+
+alpha = 0.01;
+num_iters = 400;
+
+% Init Theta and Run Gradient Descent 
+theta = zeros(3, 1);
+[theta, J_history] = gradientDescentMultivariate(X, y, theta, alpha, num_iters);
+
+% Plot the convergence graph
+figure;
+plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+xlabel('Number of iterations');
+ylabel('Cost J');
+
+% Display gradient descent's result
+fprintf('Theta computed from gradient descent: \n');
+fprintf(' %f \n', theta);
+fprintf('\n');fprintf('Solving with normal equations...\n');
+
+%% Load Data
+data = csvread('ex1data2.txt');
+X = data(:, 1:2);
+y = data(:, 3);
+m = length(y);
+
+% Add intercept term to X
+X = [ones(m, 1) X];
