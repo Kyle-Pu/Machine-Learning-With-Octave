7dea23203af7e3084e19919b870ed0ef56b52056
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-07-27 22:31:26 -0700

    Create featureNormalization.m

---
+++ Linear Regression/Multivariate/featureNormalization.m
@@ -0,0 +1,23 @@
+function [X_normalized, means, standardDeviations] = featureNormalization(X)
+
+% These lines just help us visualize the purpose and size of each matrix
+means = zeros(1, size(X, 2));
+standardDeviations = zeros(1, size(X, 2));
+
+% Calculate the mean of each feature (variable) in the dataset
+sums = sum(X, 1);
+means = sums / size(X, 1);
+
+% Calculate the standard deviation of each feature
+for i = 1 : size(X, 2),
+  standardDeviations(i) = std(X(:, i));
+endfor
+
+% Calculate the normalized matrix of variable values (subtract each data value with the mean of the feature (each data value represents a certain feature) and divide by the feature's standard deviation).
+% To further explain, X is a matrix of data values in which each row represents a specific training example and each column is a specific feature.
+X_normalized = X - mu;
+for i = 1 : size(X, 2),
+  X_normalized(:, i) = X_temp(: , i)./sigma(i);
+endfor
+
+endfunction
