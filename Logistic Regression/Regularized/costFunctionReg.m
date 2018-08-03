70603177ddbaa2877734d0bc00a7304c3f6a3609
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-08-03 11:19:52 -0700

    Create costFunctionReg.m

---
+++ Logistic Regression/Regularized/costFunctionReg.m
@@ -0,0 +1,17 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+
+m = length(y); % Number of training examples
+grad = zeros(size(theta));
+
+
+% Calculating our hypothesis vector which we'll need for both the cost and gradient
+hypothesis = sigmoid(X * theta);
+
+% Returning the correct cost
+theta(1) = 0; % We don't want to regularize the bias feature, so theta(1) will be set to 0
+J = (1/m) * sum((-y)' * log(hypothesis) - (1 - y)' * log(1 - hypothesis)) + (lambda / (2 * m)) * (theta' * theta);
+
+% Returning the correct gradient 
+grad = ((1/m) * (X' * (hypothesis - y))) + ((lambda/m) * theta);
+
+end
