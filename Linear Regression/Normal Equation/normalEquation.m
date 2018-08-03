7e0330a327e2edddaf61014a13d89c7f916f6914
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-08-03 11:23:42 -0700

    Update normalEquation.m

--- Linear Regression/Normal Equation/normalEquation.m
+++ Linear Regression/Normal Equation/normalEquation.m
@@ -5,4 +5,4 @@ m = length(y);                    % The number of training examples in our data
 theta = zeros(size(X, 2), 1);     % Technically an unnecessary line of code. However, this allows us to better visualize what matrix theta is (its size and its purpose)
 theta = inverse(X' * X) * X' * y;
 
-endfunction
+end
