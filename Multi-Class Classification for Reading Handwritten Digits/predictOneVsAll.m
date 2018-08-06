8cff8fd9f4df929d51bda805ed0f86cf5b8d06a5
Kyle Pu <31393170+Kyle-P@users.noreply.github.com>
2018-08-05 22:22:10 -0700

    Update predictOneVsAll.m
    Add a clarifying statement

--- Multi-Class Classification for Reading Handwritten Digits/predictOneVsAll.m
+++ Multi-Class Classification for Reading Handwritten Digits/predictOneVsAll.m
@@ -9,7 +9,7 @@ p = zeros(m, 1);
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
 
-hypotheses = sigmoid(X * all_theta');
-[values, p] = max(hypotheses, [], 2);
+hypotheses = sigmoid(X * all_theta'); % Calculate our hypothesis matrix
+[values, p] = max(hypotheses, [], 2); % Save the index of the max value into p. It just so happens that in this program, the indices correspond to the data values (numbers) we are trying to predict. Therefore, we don't have to use some conversion or lookup to find out the value that should be returned as the prediction.
 
 end
